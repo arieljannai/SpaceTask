e7ddcb959624493f27660570ebbbc7e60518d275
Ariel Jannai <arieljannai@gmail.com>
2018-12-01 01:00:10 +0200

    prefabs creation works now

--- Assets/Scripts/BrokenShipPart.cs
+++ Assets/Scripts/BrokenShipPart.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class BrokenShipPart : MonoBehaviour {
 
-	void Start()
+    public static Object[] brokenShipParts;
+
+    void Awake()
+    {
+        if (brokenShipParts == null)
+        {
+            brokenShipParts = Resources.LoadAll("BrokenShipParts", typeof(Sprite));
+        }
+    }
+
+    void Start()
 	{
-		
-	}
+        int index = Random.Range(0, brokenShipParts.Length);
+        this.GetComponent<SpriteRenderer>().sprite = Instantiate(brokenShipParts[index]) as Sprite;
+    }
 	
 	void Update()
 	{
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,14 +6,35 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
 
     public GameObject player;
+    public GameObject prefabsParent;
     public int NUMBER_OF_OBSTACLES;
     public int NUMBER_OF_BROKEN_PARTS;
-    
-	void Start()
+    public float distanceFromTopEdge;
+    public float distanceFromBottomEdge;
+    public float distanceFromRightEdge;
+    public float distanceFromLeftEdge;
+    public GameObject borderTop;
+    public GameObject borderBottom;
+    public GameObject borderRight;
+    public GameObject borderLeft;
+
+    private float topObjectsLimit;
+    private float bottomObjectsLimit;
+    private float rightObjectsLimit;
+    private float leftObjectsLimit;
+
+    void Awake()
+    {
+        this.topObjectsLimit = this.borderTop.transform.position.y - this.distanceFromTopEdge;
+        this.bottomObjectsLimit = this.borderBottom.transform.position.y + this.distanceFromBottomEdge;
+        this.rightObjectsLimit = this.borderRight.transform.position.x - this.distanceFromRightEdge;
+        this.leftObjectsLimit = this.borderLeft.transform.position.x + this.distanceFromLeftEdge;
+    }
+
+    void Start()
 	{
-        // TODO: check why it's throwing an error
-        //this.PutRandomResourcesOnScreen("meteor", NUMBER_OF_OBSTACLES);
-        //this.PutRandomResourcesOnScreen("collect-1", NUMBER_OF_BROKEN_PARTS);
+        this.PutRandomResourcesOnScreen("Obstacle", NUMBER_OF_OBSTACLES);
+        this.PutRandomResourcesOnScreen("BrokenShipPart", NUMBER_OF_BROKEN_PARTS);
     }
 	
 	void Update()
@@ -33,10 +54,13 @@ public class GameController : MonoBehaviour {
         {
             GameObject objResource = Instantiate(Resources.Load(resourceName)) as GameObject;
 
-            float xPos = Random.Range(-19f, 19f);
-            float yPos = Random.Range(-8f, 11f);
+            float xPos = Random.Range(this.leftObjectsLimit, this.rightObjectsLimit);
+            float yPos = Random.Range(this.bottomObjectsLimit, this.topObjectsLimit);
+
+            // TODO: different sizes for the objects
 
             objResource.transform.position = new Vector3(xPos, yPos, 0);
+            objResource.transform.parent = prefabsParent.transform;
 
             float randAngle = Random.Range(0, 200f);
             objResource.transform.eulerAngles = new Vector3(0, 0, randAngle);
