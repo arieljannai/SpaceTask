6c22e91ad1bad4355375d9a265e06e0e5584dd2a
Ariel Jannai <arieljannai@gmail.com>
2018-11-30 11:44:03 +0200

    adding camera controller script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,20 +9,19 @@ public class CameraController : MonoBehaviour {
     public Camera selfCamera;
 
     private float x_offset;
-    private float leftmost;
     private Vector3 origPosition;
 
 	void Start()
 	{
-        this.leftmost = this.transform.position.x;
         this.origPosition = this.transform.position;
-        this.x_offset = this.leftmost - this.player.transform.position.x;// - this.background.transform.position.x;
+        this.x_offset = this.transform.position.x - this.player.transform.position.x;// - this.background.transform.position.x;
 	}
 	
 	void LateUpdate()
 	{
-        float leftEdge = Mathf.Min(this.leftmost, this.player.transform.position.x);
-        float rightEdge = Mathf.Max(this.background.transform.position.x, this.player.transform.position.x);
+        //Vector3 backgroundPosition = this.background.transform.position;
+        float leftEdge = Mathf.Min(this.transform.position.x, (23 - (this.selfCamera.orthographicSize / 2)));
+        //float rightEdge = Mathf.Max(this.background.transform.position.x, this.player.transform.position.x);
         float newX = leftEdge + this.x_offset;
         this.transform.position = new Vector3(newX, this.transform.position.y, this.transform.position.z);
     }
